722c46ebabb2d9677f4c632c82fe73b31d7b42c8
poiz1245 <139199211+poiz1245@users.noreply.github.com>
2024-03-23 17:51:35 +0900

    feat : Deafault Input Actions UIOnOff 추가, UIOnOff 스크립트 추가
    오른쪽 컨트롤러 N버튼으로 UI 끄고 키고 하는 기능 추가

---
+++ My project/Assets/Scripts/UIOnOff.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class UIOnOff : MonoBehaviour
+{
+    public InputActionReference uiOnOff;
+
+    int count = 0;
+
+    void Update()
+    {
+        uiOnOff.action.performed += UiOnOff;
+ 
+    }
+
+    void UiOnOff(InputAction.CallbackContext obj)
+    {
+        if(count == 0 )
+        {
+            gameObject.SetActive(true);
+            count ++;
+        }
+        else if(count == 1 )
+        {
+            gameObject.SetActive(false);
+            count --;
+        }
+    }
+}
