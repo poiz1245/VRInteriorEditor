b34f7b57d18cd46fbc1da9042cae9cb955bd3519
poiz1245 <139199211+poiz1245@users.noreply.github.com>
2024-04-08 16:21:55 +0900

    feat : State참조 및 currentState 값 세팅
    -State 참조
    -currentState값을 Build로 세팅

--- My project/Assets/Scripts/SelectFloorObject.cs
+++ My project/Assets/Scripts/SelectFloorObject.cs
@@ -6,12 +6,13 @@ public class SelectFloorObject : MonoBehaviour
 {
     [SerializeField] GameObject selectedObjectPreview;
     [SerializeField] GameObject selectedObject;
-
+    [SerializeField] State stateManager;
 
     public void SelectButton()
     {
         PreviewSpawner.Instance.selectedObjectCategory = 2;
         PreviewSpawner.Instance.objPreviewPrefab = selectedObjectPreview;
         ObjectSpawner.Instance.selectedObject = selectedObject;
+        stateManager.currentState = State.CurrentState.Build;
     }
 }
--- My project/Assets/Scripts/SelectWallObject.cs
+++ My project/Assets/Scripts/SelectWallObject.cs
@@ -6,12 +6,13 @@ public class SelectWallObject : MonoBehaviour
 {
     [SerializeField] GameObject selectedObjectPreview;
     [SerializeField] GameObject selectedObject;
+    [SerializeField] State stateManager;
 
     public void SelectButton()
     {
         PreviewSpawner.Instance.selectedObjectCategory = 1;
         PreviewSpawner.Instance.objPreviewPrefab = selectedObjectPreview;
         ObjectSpawner.Instance.selectedObject = selectedObject;
-
+        stateManager.currentState = State.CurrentState.Build;
     }
 }
