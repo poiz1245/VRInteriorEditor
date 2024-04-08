59665b8a91d21858a903e02d645d59c75dd92ec0
poiz1245 <139199211+poiz1245@users.noreply.github.com>
2024-04-08 17:43:32 +0900

    feat : 트리거 버튼 클릭 이벤트 추가

--- My project/Assets/Scripts/EditMode.cs
+++ My project/Assets/Scripts/EditMode.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class EditMode : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public InputActionReference triggerButtonClick;
     void Start()
     {
-        
+        triggerButtonClick.action.performed += TriggerButtonClick;
     }
 
-    // Update is called once per frame
-    void Update()
+    void TriggerButtonClick(InputAction.CallbackContext obj)
     {
-        
+        if (PreviewSpawner.Instance.isActive)
+        {
+
+        }
     }
 }
