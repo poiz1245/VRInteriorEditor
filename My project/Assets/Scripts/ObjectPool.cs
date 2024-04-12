1c9a4b336bfc4a83d8780a8048c82c77a36e3cf0
poiz1245 <139199211+poiz1245@users.noreply.github.com>
2024-04-13 03:19:00 +0900

    feat : 실제 배치 될 오브젝트의  풀 생성

---
+++ My project/Assets/Scripts/ObjectPool.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    public static ObjectPool Instance;
+
+    [SerializeField] GameObject[] objectPrefab;
+
+    List<GameObject>[] objectPool;
+
+    public int objectNumber;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        Initialize();
+    }
+
+    private void Initialize()
+    {
+        objectPool = new List<GameObject>[objectPrefab.Length];
+
+        for (int i = 0; i < objectPool.Length; i++)
+        {
+            objectPool[i] = new List<GameObject>();
+        }
+    }
+
+    public static GameObject GetObject(int index)
+    {
+        GameObject select = null;
+
+        foreach (GameObject obj in Instance.objectPool[index])
+        {
+            if (!obj.activeSelf)
+            {
+                select = obj;
+                select.SetActive(true);
+                break;
+            }
+        }
+
+        if (!select)
+        {
+            select = Instantiate(Instance.objectPrefab[index], Instance.transform);
+            Instance.objectPool[index].Add(select);
+        }
+
+        return select;
+    }
+
+    public static void ReturnObject(GameObject obj)
+    {
+        obj.gameObject.SetActive(false);
+        //Instance.previewObjectQueue.Enqueue(obj);
+    }
+}
